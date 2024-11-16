b3815371e423cd59129b879160832b88c9917bd3
Atharva <angorule@gmail.com>
2024-11-17 01:40:02 +0530

    AI Added

--- app/src/main/java/com/example/aisplitwise/AISplitwiseImageAcceptorActivity.kt
+++ app/src/main/java/com/example/aisplitwise/AISplitwiseImageAcceptorActivity.kt
@@ -18,7 +18,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -27,10 +35,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,8 +48,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
@@ -47,12 +60,42 @@ import coil.request.ImageRequest
 import coil.request.SuccessResult
 import com.example.aisplitwise.ui.theme.AISplitwiseTheme
 import com.example.aisplitwise.utils.ifNullOrEmpty
+import com.google.firebase.Firebase
+import com.google.firebase.app
+import com.google.firebase.vertexai.type.Schema
+import com.google.firebase.vertexai.type.content
+import com.google.firebase.vertexai.type.generationConfig
+import com.google.firebase.vertexai.vertexAI
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+val transactionSchema = Schema.obj(
+    mapOf(
+        "sender" to Schema.obj(
+            mapOf(
+                "name" to Schema.string(),
+                "upiId" to Schema.string()
+            )
+        ),
+        "receiver" to Schema.obj(
+            mapOf(
+                "name" to Schema.string(),
+                "upiId" to Schema.string()
+            )
+        ),
+        "amount" to Schema.double(),
+        "time" to Schema.string("Iso Time"), // ISO 8601 format
+        "platform" to Schema.enumeration(listOf("GooglePay", "PhonePe", "Paytm", "Other"))
+    )
+)
+
+val generativeModel = Firebase.vertexAI.generativeModel("gemini-1.5-flash",generationConfig = generationConfig {
+    responseMimeType = "application/json"
+    responseSchema = transactionSchema
+})
 
 class AISplitwiseImageAcceptorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,8 +103,11 @@ class AISplitwiseImageAcceptorActivity : ComponentActivity() {
         enableEdgeToEdge()
         val imageUri =  intent.extras?.get(Intent.EXTRA_STREAM).toString()
 
+
+
         setContent {
             AISplitwiseTheme {
+
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -105,12 +151,18 @@ fun ImageAcceptorScreen(imageUri:String) {
         val result = (loader.execute(request) as? SuccessResult)?.drawable
         imageBitmap = drawableToBitmap(result)
         imageBitmap?.let { bitmap ->
-            processImage(bitmap,onTextRecognized= { text ->
-                recognizedText = text
-                saveTextToScopedStorage(context,recognizedText,resultLauncher)
-                saveRecognizedTextToFile(context,text)
-            },
-                error = {})
+
+            val prompt = content {
+                image(bitmap)
+                text("Extract Sender Name,Sender UPI Id ,Reciever Name ,Reciever UPI Id,Amount ,Time in iso")
+            }
+            recognizedText=generativeModel.generateContent(prompt).text?:""
+//            processImage(bitmap,onTextRecognized= { text ->
+//                recognizedText = text
+//                saveTextToScopedStorage(context,recognizedText,resultLauncher)
+//                saveRecognizedTextToFile(context,text)
+//            },
+//                error = {})
         }
 
     }
@@ -306,5 +358,47 @@ fun saveTextToScopedStorage(context: Context, recognizedText: String, resultLaun
         saveRecognizedTextToPublicDocuments(context, recognizedText)
     }
 }
+
+@Preview(device = Devices.NEXUS_5, showBackground = true)
+@Composable
+fun GradientBackgroundPreview(){
+    AISplitwiseTheme  {
+        val infiniteTransition = rememberInfiniteTransition()
+
+        val red by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 3000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val green by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 4000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val blue by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 5000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+//        Box(
+//            Modifier
+//                .fillMaxSize()
+//                .background(gradientBrush)
+//        )
+    }
+}
+
 
 
