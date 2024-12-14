b198d81f65e062211ebffac5eb18f1e9ad3c60bd
Atharva <angorule@gmail.com>
2024-12-15 00:17:02 +0530

    CI-CD started

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.serialization)
     alias(libs.plugins.google.services)
     alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.kotlin.compose)
+
     id ("kotlin-parcelize")
 
 }
@@ -39,11 +41,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility =JavaVersion.VERSION_17
+                targetCompatibility =JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
